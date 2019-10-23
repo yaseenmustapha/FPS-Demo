7bef27fde253dcbcf9d42f0260b16910f015390a
yaseenmustapha <yaseenmust@gmail.com>
2019-10-22 22:14:17 -0700

    Created character controller

---
+++ Assets/Game/Scripts/LookX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour {
+    private float _sensitivity = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float _mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRot = transform.localEulerAngles;
+        newRot.y += _mouseX * _sensitivity;
+        transform.localEulerAngles = newRot;
+	}
+}
---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = 9.81f;
+
+	// Use this for initialization
+	void Start () {
+        _controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CalculateMovement();
+	}
+
+    void CalculateMovement() {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+
+        velocity = transform.transform.TransformDirection(velocity);
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
